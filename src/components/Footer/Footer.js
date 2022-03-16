605e4c1afb5f551176d5c826f2111303f8a61a6b
Ben <benbperry1@gmail.com>
2022-03-16 11:32:10 -0400

    add favicon

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css' 
+import { Link } from 'react-router-dom'
+import { GrFacebook } from 'react-icons/gr'
+import { TiSocialInstagram } from 'react-icons/ti'
+import { MdEmail } from 'react-icons/md'
+
+
+function Footer() {
+  
+  return (
+    <div className='footer-container'>
+        
+        
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { GrFacebook } from 'react-icons/gr'
+import { TiSocialInstagram } from 'react-icons/ti'
+import { MdEmail } from 'react-icons/md'
+
+
+function Header() {
+  
+  return (
+    <div>
+    <h1>Ben Perry</h1>
+    </div>
+  );
+}
+
+export default Header;
